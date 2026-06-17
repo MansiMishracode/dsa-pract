6029c6deec69c730dce19bc56644d13b803a60de
MansiMishracode <mansimishratechnical@gmail.com>
2026-06-17 21:50:20 +0530

    stl prac.cpp

--- pract.cpp
+++ pract.cpp
@@ -68,18 +68,40 @@ void print7(int n){
 void print8(int n){
     for(int i=0;i<n;i++){
         //space
-        for(int j=0;j<n-i-1;j++){
+        for(int j=0;j<i;j++){
             cout<< " ";
         }
         //star
-        for(int j=0;j<2*i+1;j++){
+        for(int j=0;j<2*n-(2*i+1);j++){
             cout<< "*";
         }
         //space
-        for(int j=0;j<n-i-1;j++){
+        for(int j=0;j<i;j++){
             cout<< " ";
         }
         cout<<endl;
+    }
+}
+void print9(int n){
+    for(int i=1;i<=2*n-1;i++){
+        int stars=i;
+        if(i>n)stars=2*n-i;
+        for(int j=1;j<=stars;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+void print10(int n){
+    for(int i=0;i<n;i++){
+         int start=1;
+         if(i%2 == 0) start=1;
+         else start =0;
+         for(int j=0;j<=i;j++){
+            cout<<start;
+            start=1-start;
+         }
+         cout<<endl;
     }
 }
 int main(){
@@ -88,7 +110,7 @@ int main(){
     for(int i=0;i<t;i++){
         int n;
         cin>>n;
-        print8(n);
+        print10(n);
     }
     return 0;
 }
